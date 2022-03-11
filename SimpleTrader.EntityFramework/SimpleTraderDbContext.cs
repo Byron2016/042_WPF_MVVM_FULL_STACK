7fcd5ab14901c01bc008952a61c290954fbeca72
Byron 2016 <bgva2005@yahoo.com>
2022-03-11 11:20:46 -0500

    feat: v2: move connectionstring to a dbcontextfactory

--- SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -14,6 +14,8 @@ namespace SimpleTrader.EntityFramework
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AssetTransaction> AssetTransactions { get; set; }
 
+        public SimpleTraderDbContext(DbContextOptions options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
@@ -21,13 +23,13 @@ namespace SimpleTrader.EntityFramework
             base.OnModelCreating(modelBuilder);
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlServer("Server=DESKTOP-S68ME9Q\\MSSQLSERVER_2019;Initial Catalog=SimpleTraderDB;User Id=bv; Password=123456;MultipleActiveResultSets=true");
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-S68ME9Q;Initial Catalog=SimpleTraderDB;persist security info=True;user id=sa;password=123456");
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //optionsBuilder.UseSqlServer("Server=DESKTOP-S68ME9Q\\MSSQLSERVER_2019;Initial Catalog=SimpleTraderDB;User Id=bv; Password=123456;MultipleActiveResultSets=true");
+        //    optionsBuilder.UseSqlServer("Data Source=DESKTOP-S68ME9Q;Initial Catalog=SimpleTraderDB;persist security info=True;user id=sa;password=123456");
 
-            base.OnConfiguring(optionsBuilder);
-        }
+        //    base.OnConfiguring(optionsBuilder);
+        //}
     }
 }
 
---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
+    {
+        public SimpleTraderDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
+            options.UseSqlServer("Data Source=DESKTOP-S68ME9Q;Initial Catalog=SimpleTraderDB;persist security info=True;user id=sa;password=123456");
+
+            return new SimpleTraderDbContext(options.Options);
+        }
+    }
+}
