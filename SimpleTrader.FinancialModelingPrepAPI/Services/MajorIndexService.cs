15ad0755706df71fc68f4d542ce8ba732f4105fd
Byron 2016 <bgva2005@yahoo.com>
2022-03-11 15:37:55 -0500

    feat: v4: call financial api

--- SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleTrader.FinancialModelingPrepAPI.Services
 {
-    public class MajorIndexService
+    public class MajorIndexService : IMajorIndexService
     {
+        public async Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://financialmodelingprep.com//api/v3/majors-indexes/.DJI?apikey=053bd0088c22d83c10c8d680215c9924");
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                MajorIndex majorIndex = JsonConvert.DeserializeObject<MajorIndex>(jsonResponse);
+
+                majorIndex.Type = indexType;
+
+                return majorIndex;
+            }
+        }
     }
 }
