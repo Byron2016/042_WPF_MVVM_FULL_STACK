9667dffca6c21a800c654f54420d64ebc411ab7f
Byron 2016 <bgva2005@yahoo.com>
2022-03-11 12:04:31 -0500

    feat: v2: test generic service for create

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.EntityFramework;
+using SimpleTrader.EntityFramework.Services;
+
+IDataService<User> userService = new GenericDataService<User>(new SimpleTraderDbContextFactory());
+
+userService.Create(new User { 
+    Username = "Test" ,
+    Email = "test@yahoo.com",
+    Password = "TestPassword",
+    DatedJoined = DateTime.UtcNow.Date
+}).Wait();
+
+
+Console.ReadLine();
