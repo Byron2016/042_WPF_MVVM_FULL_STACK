97ff7aab1e96ceda1385656c2151d8bf5f854fc0
Byron 2016 <bgva2005@yahoo.com>
2022-03-11 11:03:04 -0500

    feat: v1: update database

--- SimpleTrader.EntityFramework/Migrations/20220311160031_add-dates_and_password.Designer.cs
+++ SimpleTrader.EntityFramework/Migrations/20220311160031_add-dates_and_password.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleTrader.EntityFramework;
 
@@ -11,9 +12,10 @@ using SimpleTrader.EntityFramework;
 namespace SimpleTrader.EntityFramework.Migrations
 {
     [DbContext(typeof(SimpleTraderDbContext))]
-    partial class SimpleTraderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220311160031_add-dates_and_password")]
+    partial class adddates_and_password
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
