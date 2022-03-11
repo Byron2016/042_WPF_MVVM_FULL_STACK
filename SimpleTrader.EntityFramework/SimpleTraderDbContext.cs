c18ca7e4ffd5526a5ac28aea16db75bf6e993868
Byron 2016 <bgva2005@yahoo.com>
2022-03-11 10:29:31 -0500

    feat: v2: add dbcontext

---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-S68ME9Q\\MSSQLSERVER_2019;Initial Catalog=SimpleTraderDB;User Id=bv; Password=123456;MultipleActiveResultSets=true");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
